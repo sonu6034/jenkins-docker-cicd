a8c5f6d30424177b3eff8376019aadf566f8d567
sonu6034 <sharmasonu00235@gmail.com>
2025-08-05 15:37:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD without Git working successful and i was intersted from indain!"
+    return "Hello from Jenkins CI/CD within Git working successful "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
